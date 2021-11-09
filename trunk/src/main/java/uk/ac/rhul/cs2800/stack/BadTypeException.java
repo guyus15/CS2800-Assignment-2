4568c78eeac50fb293cb71c4100e0e93cb91203c
zjac142 <zjac142@d43f8fa8-6ea0-4598-bbe6-1391aa7c0841>
2021-11-09 20:10:50 +0000

    Added previously written Stack class with tests to the repository.

---
+++ trunk/src/main/java/uk/ac/rhul/cs2800/stack/BadTypeException.java
@@ -0,0 +1,20 @@
+package uk.ac.rhul.cs2800.stack;
+
+/**
+ * This exception should be thrown when a given type is not expected.
+ *
+ * @author chamb
+ */
+public class BadTypeException extends Exception {
+
+  private static final long serialVersionUID = 1L;
+
+  /**
+   * Creates a new instance of a BadTypeException.
+   *
+   * @param errorMessage The error message to be shown when the error is thrown.
+   */
+  public BadTypeException(String errorMessage) {
+    super(errorMessage);
+  }
+}
