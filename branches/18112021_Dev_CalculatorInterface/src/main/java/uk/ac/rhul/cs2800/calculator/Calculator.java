cf58b7c7e82c13bd7cc885d6754a0d814cee3256
zjac142 <zjac142@d43f8fa8-6ea0-4598-bbe6-1391aa7c0841>
2021-11-18 02:27:29 +0000

    Added the Calculator interface. Testing will be done when we begin to implement the Calculator interface within the code base.

--- branches/18112021_Dev_CalculatorInterface/src/main/java/uk/ac/rhul/cs2800/calculator/Calculator.java
+++ branches/18112021_Dev_CalculatorInterface/src/main/java/uk/ac/rhul/cs2800/calculator/Calculator.java
@@ -1,5 +1,17 @@
 package uk.ac.rhul.cs2800.calculator;
 
+/**
+ *  Allows the calculation of String expressions. 
+ *
+ * @author chamb
+ */
 public interface Calculator {
 
+  /**
+   * Called when we want to evaluate an arithmetic expression String.
+   *
+   * @param stringToEvaluate the String to be evaluated.
+   * @return the evaluation of the String
+   */
+  public float evaluate(String stringToEvaluate);  
 }
