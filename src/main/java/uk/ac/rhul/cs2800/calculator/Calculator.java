9d1860c3d897ea0b401b5268df62f88a17c96e7e
Guy Chamberlain-Webber <41839925+guyus15@users.noreply.github.com>
2021-11-20 00:00:42 +0000

    Implemented a Singleton design pattern into the Calculator class.

--- src/main/java/uk/ac/rhul/cs2800/calculator/Calculator.java
+++ src/main/java/uk/ac/rhul/cs2800/calculator/Calculator.java
@@ -5,13 +5,34 @@ package uk.ac.rhul.cs2800.calculator;
  *
  * @author chamb
  */
-public interface Calculator {
+public final class Calculator {
 
+  // Does not need to be public as all access to the Calculator is done via getInstance().
+  private Calculator() {}
+  
+  public static Calculator instance = null;
+  
+  /**
+   * Accessing the Calculator Singleton pattern. The first time this is called
+   * a new instance of a Calculator will be created.
+   *
+   * @return the current instance of the Calculator Singleton.
+   */
+  public static Calculator getInstance() {
+    if (instance == null) {
+      instance = new Calculator();
+    }
+    
+    return instance;
+  }
+  
   /**
    * Called when we want to evaluate an arithmetic expression String.
    *
    * @param stringToEvaluate the String to be evaluated.
-   * @return the evaluation of the String
+   * @return the evaluation of the arithmetic expression String
    */
-  public float evaluate(String stringToEvaluate);  
+  public float evaluate(String stringToEvaluate) {
+    return 0;
+  }
 }
