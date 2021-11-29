7c3285dd68b1a63f80d674691ebed7b8c17c48fc
zjac142 <zjac142@d43f8fa8-6ea0-4598-bbe6-1391aa7c0841>
2021-11-29 00:37:15 +0000

    Added an AsciiViewController class which implements the view interface. This is responsible for getting user input and responding with appropriate output when the program is run via the command-line.

---
+++ branches/25112021_Dev_ViewAssembly/src/main/java/uk/ac/rhul/cs2800/calculator/AsciiViewController.java
@@ -0,0 +1,52 @@
+package uk.ac.rhul.cs2800.calculator;
+
+import java.util.Scanner;
+
+/**
+ * The main view for the command-line version of the Calculator. Along with the GuiView,
+ * this will be the main way that user's interact with the application.
+ *
+ * @author chamb
+ */
+public class AsciiViewController implements View {
+  
+  Scanner scanner;
+  
+  /**
+   * Creates an instance of the {@code AsciiViewController}.
+   */
+  public AsciiViewController() {
+    scanner = new Scanner(System.in);
+  }
+  
+  /**
+   * Gets from input the expression that the user would like to evaluate.
+   */
+  public String getExpression() {
+    System.out.println("Enter an expression to be evaluated: ");
+    return scanner.nextLine();
+  }
+  
+  /**
+   * Outputs the value of a successfully evaluated expression.
+   */
+  public void setAnswer(String str) {
+    System.out.println(str);
+  }
+  
+  /**
+   * Add a {@code Calculator} observer.
+   */
+  public void addCalcObserver() {
+    return;
+  }
+
+  
+  /**
+   * Add a {@code Type} observer.
+   */
+  public void addTypeObserver() {
+    return;
+  }
+  
+}
