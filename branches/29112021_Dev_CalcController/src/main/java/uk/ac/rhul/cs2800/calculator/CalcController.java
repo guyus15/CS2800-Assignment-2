d6b935553d456fdfb1fb7a79017c80a7c7ad2975
zjac142 <zjac142@d43f8fa8-6ea0-4598-bbe6-1391aa7c0841>
2021-12-01 23:45:22 +0000

    Create the CalcController class which acts as an observer to observe changes to the view interface.

---
+++ branches/29112021_Dev_CalcController/src/main/java/uk/ac/rhul/cs2800/calculator/CalcController.java
@@ -0,0 +1,11 @@
+package uk.ac.rhul.cs2800.calculator;
+
+/**
+ * Responsible for linking the view and model together. Adds itself as an observer
+ * to the view so that any changes to it can be observed.
+ *
+ * @author chamb
+ */
+public class CalcController {
+  
+}
