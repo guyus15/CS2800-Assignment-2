bfc27f4ffa6517abe43affe2b75f11d2b9717e20
Guy Chamberlain-Webber <41839925+guyus15@users.noreply.github.com>
2021-12-05 02:56:09 +0000

    Create the CalcModel class which implements a state design pattern to managing switching between a StandardCalc and RevPolishCalc depending on the current state.

---
+++ src/main/java/uk/ac/rhul/cs2800/calculator/CalcModel.java
@@ -0,0 +1,56 @@
+package uk.ac.rhul.cs2800.calculator;
+
+/**
+ * Is a State design pattern designed to encapsulate RevPolishCalc and
+ * StandardCalc classes so they can be used under the same class. 
+ *
+ * @author chamb
+ */
+public class CalcModel {
+  
+  Calculator currentCalc;
+  StandardCalc standardCalc;
+  RevPolishCalc revPolishCalc;
+  
+  /**
+   * The constructor for the CalcModel class.
+   */
+  public CalcModel() {
+    standardCalc = new StandardCalc();
+    revPolishCalc = new RevPolishCalc();
+    
+    currentCalc = standardCalc; // By default the state of the calculator is infix.
+  }
+ 
+  /**
+   * Toggles the state of the calculator between 
+   * standard and reverse Polish.
+   *
+   * @param isInfix a boolean determining if the calculator should be infix or not. 
+Will be postfix if this is false.
+   */
+  public void setType(boolean isInfix) {
+    if (isInfix) {
+      currentCalc = standardCalc;
+    } else {
+      currentCalc = revPolishCalc;
+    }
+  }
+  
+  /**
+   * Get the result of the arithmetic expression string.
+   *
+   * @param expression the expression to be evaluated.
+   * @return the evaluation of the string expression.
+   * @throws BadTypeException never gets thrown due to the use of facade.
+   * @throws EmptyStackException when called if the stack size is zero.
+   * @throws InvalidExpressionException thrown if the given expression is invalid.
+   */
+  public float evaluate(String expression) throws
+      InvalidExpressionException,
+      EmptyStackException,
+      BadTypeException {
+    return currentCalc.evaluate(expression);
+  }
+  
+}
