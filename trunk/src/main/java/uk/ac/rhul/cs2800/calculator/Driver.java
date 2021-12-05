2b72fd51943c5501e770f451292455e4b7195f6e
zjac142 <zjac142@d43f8fa8-6ea0-4598-bbe6-1391aa7c0841>
2021-12-05 16:26:55 +0000

    Merged the 25112021_Dev_ViewAssembly branch back to the trunk.

---
+++ trunk/src/main/java/uk/ac/rhul/cs2800/calculator/Driver.java
@@ -0,0 +1,37 @@
+package uk.ac.rhul.cs2800.calculator;
+
+/**
+ * The entry point for the program, logic regarding how the program will appear (GUI or ASCII view)
+ * will stem from here.
+ *
+ * @author chamb
+ */
+public class Driver {
+
+  /**
+   * The main method for the Driver class.
+   *
+   * @param args the program's arguments.
+   */
+  public static void main(String[] args) {
+
+    // This determines if the program has been run using a GUI.
+    if (System.console() == null) {
+      // Launch GUI application
+      System.out.println("Running the GUI application.");
+      
+      // Initiates a new thread to run the GUI view application on.
+      // This code allows the decoupling of the main class and the GuiView class.
+      new Thread() {
+        @Override
+        public void run() {
+          javafx.application.Application.launch(GuiView.class);
+        }
+      }.start();
+            
+    } else {
+      // Launch ASCII application
+      System.out.println("Running the ASCII application.");
+    }
+  }
+}
